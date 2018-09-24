351bd352f671427f1b822877035b4b136d943adb
mbenezra18 <michayla.benezra@gmail.com>
2018-09-23 23:07:52 -0400

    Add files via upload

--- githubAPI.py
+++ githubAPI.py
@@ -1,14 +1,16 @@
-'''
+"""
 @author Michayla Ben-Ezra
 September 23, 2018
-
 SSW567 - HW04
-'''
+
+I pledge my honor that I have abided by the Stevens Honor System.
+"""
 
 import requests
 import json
 
-def GetRepos(user_id):
+
+def get_repos(user_id):
 
     r = requests.get('https://api.github.com/users/' + user_id + '/repos')
     repos = []
@@ -17,24 +19,26 @@ def GetRepos(user_id):
     for i in repos_info:
         try:
             repos += [i.get('name')]
-        except (attributeError):
-            print ('Error: This user does not have any repositories.')
+        except AttributeError:
+            print('Error: This user does not have any repositories.')
             return []
         return repos
 
-def retrieve_commits(user_id, Repository):
-    repo_commits = requests.get('https://api.github.com/repos/' +user_id+ '/' +Repository+ 'commits')
-    pushTo_json = json.load(repo_commits.text)
+
+def retrieve_commits(user_id, repository):
+
+    repo_commits = requests.get('https://api.github.com/repos/' + user_id + '/' + repository + 'commits')
+    pushTo_json = json.loads(repo_commits.text)
 
     return len(pushTo_json)
+
 
 def main():
 
     user_id = input("Please input a GitHub username:")
 
-
-    repos = Repository(user_id)
+    repos = get_repos(user_id)
 
     if len(repos) > 0:
-        for i in repos:
-            print("Repository: " +r+ "Number of Commits: " +str(retrieve_commits(user_id, r)))
+       for i in repos:
+            print("Repository: " + repos + "Number of Commits: " + str(retrieve_commits(user_id, repos)))
